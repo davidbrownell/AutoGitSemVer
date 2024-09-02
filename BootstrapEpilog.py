98562f944e7d27de6208943bf543007cb99bb81a
David Brownell <db@DavidBrownell.com>
2024-09-02 11:12:01 -0400

    👽️ [external_api_change] Updated to copier-ProjectScaffolding v0.2.6

--- BootstrapEpilog.py
+++ BootstrapEpilog.py
@@ -1,50 +1,27 @@
-# ----------------------------------------------------------------------
-# |
-# |  BootstrapEpilog.py
-# |
-# |  David Brownell <db@DavidBrownell.com>
-# |      2023-12-18 15:26:50
-# |
-# ----------------------------------------------------------------------
-# |
-# |  Copyright David Brownell 2023-24
-# |  Distributed under the MIT License.
-# |
-# ----------------------------------------------------------------------
+# pylint: disable=missing-module-docstring
+
 import os
 import subprocess
 import sys
 
 from pathlib import Path
 
+
 # Parse the arguments
-is_debug = False
-is_force = False
-is_verbose = False
 is_package = False
 no_cache = False
 
 display_flags: list[str] = []
 
-for arg in sys.argv[
-    2:
-]:  # First arg is the script name, second arg is the name of the shell script to write to
-    if arg == "--debug":
-        is_debug = True
-    elif arg == "--force":
-        is_force = True
-    elif arg == "--verbose":
-        is_verbose = True
-    elif arg == "--package":
+# First arg is the script name, second arg is the name of the shell script to write to
+for arg in sys.argv[2:]:
+    if arg == "--package":
         is_package = True
         display_flags.append("package")
     elif arg == "--no-cache":
         no_cache = True
     else:
-        raise Exception("Unrecognized argument: {}".format(arg))
-
-if is_debug:
-    is_verbose = True
+        sys.stderr.write(f"WARNING: '{arg}' is not a recognized argument.\n")
 
 subprocess.run(
     'pip install --disable-pip-version-check {} --editable ".[dev{}]"'.format(
